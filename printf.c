1a71b4c7ad53d9f922e08e23b1dad12e885f5c48
Marsiya Issah <masy370@gmail.com>
2023-03-25 19:39:56 -0400

    Update printf.c file

--- printf.c
+++ printf.c
@@ -10,21 +10,44 @@
  **/
 int _printf(const char *format, ...)
 {
-	int size;
+	{
+	int count = 0;
 	va_list args;
-
-	if (format == NULL)
-		return (-1);
-
-	size = _strlen(format);
-	if (size <= 0)
-		return (0);
+	int (*function)(va_list) = NULL;
 
 	va_start(args, format);
-	size = handler(format, args);
 
-	_putchar(-1);
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1) != '%')
+		{
+			format++;
+			function = get_function(format);
+			if (*(format) == '\0')
+				return (-1);
+			else if (function == NULL)
+			{
+				_putchar(*(format - 1));
+				_putchar(*format);
+				count += 2;
+			}
+			else
+				count += function(args);
+		}
+		else if (*format == '%' && *(format + 1) == '%')
+		{
+			format++;
+			_putchar('%');
+			count++;
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+
+		format++;
+	}
 	va_end(args);
-
-	return (size);
+	return (count);
 }
