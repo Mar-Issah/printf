944f9a89fc876048bd3a6f338ffff6640bfd1a24
Mar-Issah <masy370@gmail.com>
2023-03-26 14:39:48 -0400

    remove handler, utils and main.c files

--- main.h
+++ main.h
@@ -1,21 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+// #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
-/* utils.c */
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
+// char *itoa(long int, int);
 
 /* printf.c */
 int _printf(const char *, ...);
-
-/* handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
 
 /* printers */
 int print_string(va_list);
@@ -32,7 +25,6 @@ int print_rev_string(va_list);
 
 /* _putchar.c */
 int _putchar(char);
-int buffer(char);
 
 /**
  * struct _format - Typedef struct
@@ -45,6 +37,5 @@ typedef struct _format
 	char type;
 	int (*f)(va_list);
 } format;
-
 
 #endif
